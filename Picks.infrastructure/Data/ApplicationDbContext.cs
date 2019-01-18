c5f4061840dcf3a4a0a4f53c144abb8e321095cf
Timmie Bark <timmie.bark@hotmail.com>
2019-01-18 16:12:14 +0100

    Added picks.infrastructure and dbContext

---
+++ Picks.infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Picks.core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picks.infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
