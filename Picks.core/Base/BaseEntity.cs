0173c1a24d24c380318178790faf1c4eedcb2b56
Timmie Bark <timmie.bark@hotmail.com>
2019-01-18 15:06:41 +0100

    Changed folderName from Picks2 to Picks and created BaseEntity

---
+++ Picks.core/Base/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picks.core.Base
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
