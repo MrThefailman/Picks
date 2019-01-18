7d0b8f3ce61a92cc643f019660cc16e3b45c3d0f
Timmie Bark <timmie.bark@hotmail.com>
2019-01-18 15:21:18 +0100

    Added image and category models

---
+++ Picks.core/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using Picks.core.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picks.core.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public ICollection<Image> Images { get; set; }
+    }
+}
---
+++ Picks.core/Entities/Image.cs
@@ -0,0 +1,15 @@
+﻿using Picks.core.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picks.core.Entities
+{
+    public class Image : BaseEntity
+    {
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public virtual int CategoryId { get; set }
+    }
+}
