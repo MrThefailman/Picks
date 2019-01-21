a69ab9b778f734b375807dd2bfcdb149fd61ff4a
Timmie Bark <timmie.bark@hotmail.com>
2019-01-21 12:59:50 +0100

    Generic repository really commiteted

---
+++ Picks.infrastructure/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picks.infrastructure.Repositories.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task Add(TEntity entity);
+        IQueryable<TEntity> Find();
+    }
+}
