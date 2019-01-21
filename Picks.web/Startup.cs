dece647ee877b4defab756b9f3ed2fec3a0cc778
Timmie Bark <timmie.bark@hotmail.com>
2019-01-21 09:58:41 +0100

    Added entityFramework

--- Picks.web/Startup.cs
+++ Picks.web/Startup.cs
@@ -7,23 +7,29 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Picks.infrastructure.Data;
 
 namespace Picks.web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        IConfiguration _configuration;
 
-        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration conf)
+        {
+            _configuration = conf;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var conn = _configuration.GetConnectionString("Picks");
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(conn));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
